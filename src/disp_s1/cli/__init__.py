7eabb5867172c23ddbecd214d8a23899dbdd0b7f
scott <scott.stanie@gmail.com>
2024-01-09 16:40:58 -0500

    switch cli to use click for easier subcommands

---
+++ src/disp_s1/cli/__init__.py
@@ -0,0 +1,20 @@
+import click
+
+from .run import run
+
+
+@click.group()
+@click.version_option()
+@click.option("--debug", is_flag=True, help="Add debug messages to the log.")
+@click.pass_context
+def cli_app(ctx: click.Context, debug: bool) -> None:
+    """Run a displacement workflow."""
+    # https://click.palletsprojects.com/en/8.1.x/commands/#nested-handling-and-contexts
+    ctx.ensure_object(dict)
+    ctx.obj["debug"] = debug
+
+
+cli_app.add_command(run)
+
+if __name__ == "__main__":
+    cli_app()

e769094ef04a2ccdcd6c7d9ff1a31439e2a22bec
ehavazli <emre.havazli@jpl.nasa.gov>
2024-01-19 16:04:12 -0800

    add  option to CLI

--- src/disp_s1/cli/create.py
+++ src/disp_s1/cli/create.py
@@ -1,5 +1,7 @@
 import click
 
+from disp_s1.create import get_params, make_product
+
 
 @click.command()
 @click.argument(
@@ -7,43 +9,8 @@ import click
 )
 @click.argument("cslc_list", type=click.File("r"))
 @click.argument("pair", type=str)
-def create(process_dir, cslc_list, pair):
+@click.argument("frame_id", type=str)
+@click.argument("processing_mode", type=str)
+def create(process_dir, cslc_list, pair, frame_id, processing_mode) -> None:
     """Create DISP-S1 product for given pair."""
-    from pathlib import Path
-
-    from disp_s1.pge_runconfig import RunConfig
-    from disp_s1.product import create_output_product
-
-    # Get necessary files.
-    prod_dict = {}
-    prod_dict["unw_filename"] = next(
-        Path(f"{process_dir}/unwrapped/").glob(f"{pair}.unw.tif")
-    )
-    prod_dict["conncomp_filename"] = next(
-        Path(f"{process_dir}/unwrapped/").glob(f"{pair}.unw.conncomp")
-    )
-    prod_dict["ifg_corr_filename"] = next(
-        Path(f"{process_dir}/interferograms/").glob(f"{pair}.cor")
-    )
-
-    prod_dict["tcorr_filename"] = next(
-        Path(f"{process_dir}/interferograms/").glob("temporal_coherence*tif")
-    )
-    prod_dict["ps_mask_filename"] = next(
-        Path(f"{process_dir}/interferograms/").glob("ps_mask_looked.tif")
-    )
-    prod_dict["pge_runconfig"] = next(Path(f"{process_dir}/").glob("*.yaml"))
-    cslc_files = sorted(cslc_list.read().splitlines())
-    prod_dict["cslc_files"] = cslc_files
-
-    create_output_product(
-        unw_filename=prod_dict["unw_filename"],
-        conncomp_filename=prod_dict["conncomp_filename"],
-        tcorr_filename=prod_dict["tcorr_filename"],
-        ifg_corr_filename=prod_dict["ifg_corr_filename"],
-        output_name=f"{pair}.nc",
-        corrections={},
-        ps_mask_filename=prod_dict["ps_mask_filename"],
-        pge_runconfig=RunConfig.from_yaml(prod_dict["pge_runconfig"]),
-        cslc_files=prod_dict["cslc_files"],
-    )
+    make_product(get_params(process_dir, cslc_list, pair, frame_id, processing_mode))

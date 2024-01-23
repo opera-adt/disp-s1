3c60c373ac4a484864607b8d977dd63b1039512d
Sara Mirzaee <smirzaee@aurora.jpl.nasa.gov>
2024-01-23 10:47:38 -0800

    add tropo/iono downloading script

---
+++ scripts/release/download_ERA5_TEC.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+import argparse
+import datetime
+import logging
+import re
+import sys
+from configparser import ConfigParser
+from pathlib import Path
+from typing import Sequence
+
+import numpy as np
+from opera_utils import get_zero_doppler_time, group_by_date
+from opera_utils._types import Bbox
+from rasterio.crs import CRS
+from rasterio.warp import transform_bounds
+
+from disp_s1.ionosphere import download_ionex_for_slcs
+from disp_s1.utils import get_frame_bbox
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def _get_cli_args():
+    parser = argparse.ArgumentParser(
+        description="download weather models and TEC files.",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+
+    parser.add_argument(
+        "--slc-files",
+        nargs=argparse.ONE_OR_MORE,
+        help="List the paths of corresponding SLC files.",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--frame-id", type=int, help="frame id of the data", required=True
+    )
+
+    parser.add_argument(
+        "--working-dir", default="./", help="working directory for outputs"
+    )
+
+    return parser
+
+
+def check_exist_grib_file(grib_files: list[Path]) -> list[Path]:
+    """Check for grib files that are already downloaded
+
+    Parameters
+    ----------
+    grib_files: list[Path]
+        list of the path to grib files
+
+    Returns
+    -------
+    list[Path]
+        list of all the grib files that already exist
+    """
+    grib_files_exist = []
+    for file in grib_files:
+        if file.exists():
+            grib_files_exist.append(file)
+    return grib_files_exist
+
+
+def dload_grib_files(
+    grib_files: list[Path], tropo_model: str = "ERA5", snwe: Bbox = None
+) -> list[Path]:
+    """Download weather re-analysis grib files using PyAPS
+
+    Parameters
+    ----------
+    grib_files list[Path]
+        list of path to the grib files
+    tropo_model str
+        the tropospheric model
+    snwe Bbox
+        bounding box of the data required in the format of (south north west east)
+
+    Returns
+    -------
+    list of path
+    """
+    import pyaps3 as pa
+
+    logger.info("downloading weather model data using PyAPS ...")
+
+    parent_dir = Path(pa.__file__).parent
+
+    # Get date list to download (skip already downloaded files)
+    grib_files_exist = check_exist_grib_file(grib_files)
+    grib_files2dload = sorted(list(set(grib_files) - set(grib_files_exist)))
+    date_list2dload = [str(re.findall(r"\d{8}", i.name)[0]) for i in grib_files2dload]
+    logger.info("number of grib files to download: %d" % len(date_list2dload))
+
+    # Download grib file using PyAPS
+    if len(date_list2dload) > 0:
+        hour = (
+            re.findall(r"\d{8}[-_]\d{2}", grib_files2dload[0].name)[0]
+            .replace("-", "_")
+            .split("_")[1]
+        )
+        grib_dir = grib_files2dload[0].parent
+
+        # Check for non-empty account info in PyAPS config file
+        check_pyaps_account_config(tropo_model, parent_dir)
+
+        # try 3 times to download, then use whatever downloaded to calculate delay
+        i = 0
+        while i < 3:
+            i += 1
+            try:
+                if tropo_model in ["ERA5", "ERAINT"]:
+                    pa.ECMWFdload(
+                        date_list2dload,
+                        hour,
+                        grib_dir,
+                        model=tropo_model,
+                        snwe=snwe,
+                        flist=grib_files2dload,
+                    )
+
+                elif tropo_model == "MERRA":
+                    pa.MERRAdload(date_list2dload, hour, grib_dir)
+
+                elif tropo_model == "NARR":
+                    pa.NARRdload(date_list2dload, hour, grib_dir)
+            except Exception:
+                if i < 3:
+                    logger.warning(
+                        f"WARNING: the {i} attempt to download failed, retry it.\n"
+                    )
+                else:
+                    logger.warning(
+                        "WARNING: downloading failed for 3 times, stop trying and continue."
+                    )
+                pass
+
+    # check potentially corrupted files
+    grib_files = check_exist_grib_file(grib_files)
+    return grib_files
+
+
+def check_pyaps_account_config(tropo_model: str, parent_dir: Path):
+    """Check for input in PyAPS config file. If they are default values or are empty, then raise error.
+
+    Parameters
+    ----------
+    tropo_model  str
+        tropo model being used to calculate tropospheric delay
+    parent_dir Path
+        Path to the PyAPS package where it is installed
+    """
+    # Convert MintPy tropo model name to data archive center name
+    # NARR model included for completeness but no key required
+    MODEL2ARCHIVE_NAME = {
+        "ERA5": "CDS",
+        "ERAI": "ECMWF",
+        "MERRA": "MERRA",
+        "NARR": "NARR",
+    }
+    SECTION_OPTS = {
+        "CDS": ["key"],
+        "ECMWF": ["email", "key"],
+        "MERRA": ["user", "password"],
+    }
+
+    # Default values in cfg file
+    default_values = [
+        "the-email-address-used-as-login@ecmwf-website.org",
+        "the-user-name-used-as-login@earthdata.nasa.gov",
+        "the-password-used-as-login@earthdata.nasa.gov",
+        "the-email-adress-used-as-login@ucar-website.org",
+        "your-uid:your-api-key",
+    ]
+
+    # account file for pyaps3 < and >= 0.3.0
+    cfg_file = parent_dir / "model.cfg"
+    rc_file = Path("~").home() / ".cdsapirc"
+
+    # for ERA5: ~/.cdsapirc
+    if tropo_model == "ERA5" and rc_file.is_file():
+        pass
+
+    # check account info for the following models
+    elif tropo_model in ["ERA5", "ERAI", "MERRA"]:
+        section = MODEL2ARCHIVE_NAME[tropo_model]
+
+        # Read model.cfg file
+        cfg_file = parent_dir / "model.cfg"
+        cfg = ConfigParser()
+        cfg.read(cfg_file)
+
+        # check all required option values
+        for opt in SECTION_OPTS[section]:
+            val = cfg.get(section, opt)
+            if not val or val in default_values:
+                msg = "PYAPS: No account info found "
+                msg += f"for {tropo_model} in {section} section in file: {cfg_file}"
+                raise ValueError(msg)
+
+    return
+
+
+def get_grib_file_names(
+    slc_files: Sequence[Path], grib_dir: Path, frame_id: int
+) -> tuple[list[Path], Bbox]:
+    """generate the grib file names for each SLC to download
+
+    Parameters
+    ----------
+
+    """
+
+    slc_file_list = group_by_date(slc_files)
+    first_date = next(iter(slc_file_list))
+    acquisition_time = get_zero_doppler_time(slc_file_list[first_date][0])
+    hour = closest_weather_model_hour(acquisition_time)
+
+    date_list = [
+        datetime.datetime.strftime(key[0], "%Y%m%d") for key in slc_file_list.keys()
+    ]
+
+    epsg, bounds = get_frame_bbox(frame_id)
+
+    if epsg != 4326:
+        bounds = transform_bounds(CRS.from_epsg(epsg), CRS.from_epsg(4326), *bounds)
+
+    snwe = (bounds[1], bounds[3], bounds[0], bounds[2])
+    area = snwe2str(snwe)
+
+    # grib file list
+    grib_files = []
+    for d in date_list:
+        if area:
+            grib_file = grib_dir / f"ERA5{area}_{d}_{hour}.grb"
+        else:
+            grib_file = grib_dir / f"ERA5_{d}_{hour}.grb"
+        grib_files.append(grib_file)
+
+    return grib_files, snwe
+
+
+def add_buffer(snwe):
+    s, n, w, e = snwe
+
+    min_buffer = 1
+    # lat/lon0/1 --> SNWE
+    S = np.floor(min(s, n) - min_buffer).astype(int)
+    N = np.ceil(max(s, n) + min_buffer).astype(int)
+    W = np.floor(min(w, e) - min_buffer).astype(int)
+    E = np.ceil(max(w, e) + min_buffer).astype(int)
+    return S, N, W, E
+
+
+def snwe2str(snwe: Bbox):
+    """Get area extent in string"""
+    if not snwe:
+        return None
+
+    S, N, W, E = add_buffer(snwe)
+
+    area = ""
+    area += f"_S{abs(S)}" if S < 0 else f"_N{abs(S)}"
+    area += f"_S{abs(N)}" if N < 0 else f"_N{abs(N)}"
+    area += f"_W{abs(W)}" if W < 0 else f"_E{abs(W)}"
+    area += f"_W{abs(E)}" if E < 0 else f"_E{abs(E)}"
+
+    return area
+
+
+def closest_weather_model_hour(sar_acquisition_time):
+    """Find closest available time of weather product from SAR acquisition time
+    Parameters: sar_acquisition_time - str, SAR data acquisition time in seconds
+                grib_source          - str, Grib Source of weather reanalysis product
+    Returns:    grib_hr              - str, time of closest available weather product
+    Example:
+        '06' = closest_weather_model_hour(atr['CENTER_LINE_UTC'])
+        '12' = closest_weather_model_hour(atr['CENTER_LINE_UTC'], 'NARR')
+    """
+    # get hour/min of SAR acquisition time
+    # sar_time = datetime.strptime(sar_acquisition_time, "%Y-%m-%d %H:%M:%S.%f").hour
+    sar_time = sar_acquisition_time.hour
+
+    # find closest time in available weather products
+    grib_hr_list = np.arange(0, 24)
+    grib_hr = int(min(grib_hr_list, key=lambda x: abs(x - sar_time)))
+
+    # add zero padding
+    grib_hr = f"{grib_hr:02d}"
+    return grib_hr
+
+
+def main(iargs=None):
+    """Download ERA5 weather model files and TEC maps for all the given SLCs."""
+    parser = _get_cli_args()
+    args = parser.parse_args(args=iargs)
+
+    grib_dir = Path(args.working_dir) / "troposphere_files"
+    grib_dir.mkdir(exist_ok=True)
+    TEC_dir = Path(args.working_dir) / "ionosphere_files"
+    TEC_dir.mkdir(exist_ok=True)
+
+    grib_files, snwe = get_grib_file_names(args.slc_files, grib_dir, args.frame_id)
+
+    dload_grib_files(grib_files, snwe=snwe)
+
+    download_ionex_for_slcs(args.slc_files, TEC_dir)
+
+    return
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- src/disp_s1/ionosphere.py
+++ src/disp_s1/ionosphere.py
@@ -4,11 +4,14 @@ from datetime import date
 from pathlib import Path
 from typing import Sequence
 
-from dolphin._types import Filename
-from dolphin.utils import group_by_date
+from opera_utils import group_by_date
+from opera_utils._types import Filename
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+
+IONOSPHERE_TYPE_JPLG = "jplg"
+IONOSPHERE_TYPE_JPRG = "jprg"
 
 
 def download_ionex_for_slcs(
@@ -39,7 +42,9 @@ def download_ionex_for_slcs(
     for input_date_tuple, file_list in date_to_file_list.items():
         input_date = input_date_tuple[0]
         logger.info("Downloading for %s", input_date)
-        f = download_ionex_for_date(input_date, dest_dir=dest_dir, verbose=verbose)
+        f = download_ionex_for_date(
+            input_date, dest_dir=dest_dir.absolute(), verbose=verbose
+        )
         output_files.append(f)
 
     return output_files
@@ -48,7 +53,7 @@ def download_ionex_for_slcs(
 def download_ionex_for_date(
     input_date: date,
     dest_dir: Filename,
-    solution_code: str = "jpl",
+    solution_code: str = "jplg",
     verbose: bool = False,
 ) -> Path:
     """Download one IONEX file for a given date.
@@ -60,7 +65,7 @@ def download_ionex_for_date(
     dest_dir : Path
         Directory to save the downloaded files.
     solution_code : str, optional
-        Analysis center code, by default "jpl".
+        Analysis center code, by default "jplg".
     verbose : bool, optional
         Print messages, by default False.
 
@@ -69,20 +74,34 @@ def download_ionex_for_date(
     Path
         Path to the local IONEX text file.
     """
-    source_url = _generate_ionex_filename(input_date, solution_code=solution_code)
-    dest_file = Path(dest_dir) / Path(source_url).name
+    source_urls = _generate_ionex_filename(input_date, solution_code=solution_code)
 
-    wget_cmd = ["wget", "--continue", "--auth-no-challenge", source_url]
+    for source_url in source_urls:
+        dest_file = Path(dest_dir) / Path(source_url).name
 
-    if not verbose:
-        wget_cmd.append("--quiet")
+        wget_cmd = ["wget", "--continue", "--auth-no-challenge", source_url]
 
-    logger.info('Running command: "%s"', " ".join(wget_cmd))
-    subprocess.run(wget_cmd, cwd=dest_dir)
+        if not verbose:
+            wget_cmd.append("--quiet")
+
+        unzipped_file = Path(dest_dir) / Path(source_url).stem
+        if unzipped_file.exists():
+            logger.info(f"{Path(source_url).name} exists")
+            continue
+
+        logger.info('Running command: "%s"', " ".join(wget_cmd))
+        subprocess.run(wget_cmd, cwd=dest_dir)
+
+        if dest_file.exists():
+            unzip_command = f"gzip --force --decompress {dest_file}".split()
+            subprocess.run(unzip_command, cwd=dest_dir)
+
     return dest_file
 
 
-def _generate_ionex_filename(input_date: date, solution_code: str = "jpl") -> str:
+def _generate_ionex_filename(
+    input_date: date, solution_code: str = "jplg"
+) -> list[str]:
     """Generate the IONEX file name.
 
     Parameters
@@ -90,7 +109,7 @@ def _generate_ionex_filename(input_date: date, solution_code: str = "jpl") -> st
     input_date : datetime
         Date to download
     solution_code : str, optional
-        GIM analysis center code, by default "jpl".
+        GIM analysis center code, by default "jplg".
     date_format : str, optional
         Date format code, by default "%Y%m%d".
 
@@ -101,7 +120,14 @@ def _generate_ionex_filename(input_date: date, solution_code: str = "jpl") -> st
     """
     day_of_year = f"{input_date.timetuple().tm_yday:03d}"
     year_short = str(input_date.year)[2:4]
-    file_name = f"{solution_code.lower()}g{day_of_year}0.{year_short}i.Z"
+    file_name = f"{solution_code.lower()}{day_of_year}0.{year_short}i.Z"
 
-    url_directory = "https://cddis.nasa.gov/archive/gnss/products/ionex"
-    return f"{url_directory}/{input_date.year}/{day_of_year}/{file_name}"
+    url_directory = f"https://cddis.nasa.gov/archive/gnss/products/ionex/{input_date.year}/{day_of_year}"
+
+    # old naming convention
+    old_archive_name = f"{url_directory}/{file_name}"
+    # new naming convention
+    product_type = "RAP" if solution_code.lower() == IONOSPHERE_TYPE_JPRG else "FIN"
+    new_archive_name = f"{url_directory}/JPL0OPS{product_type}_{input_date.year}{day_of_year}0000_01D_02H_GIM.INX.gz"
+
+    return [old_archive_name, new_archive_name]

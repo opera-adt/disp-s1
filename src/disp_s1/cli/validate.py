aab9a6ac45a2c94528f4571c829dc65e5d8f0467
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2024-08-06 10:55:46 -0700

    Add `--debug`, logging to `disp-s1 validate

--- src/disp_s1/cli/validate.py
+++ src/disp_s1/cli/validate.py
@@ -1,4 +1,5 @@
 import click
+from dolphin import setup_logging
 
 from disp_s1.validate import DSET_DEFAULT, compare
 
@@ -7,6 +8,8 @@ from disp_s1.validate import DSET_DEFAULT, compare
 @click.argument("golden", type=click.Path(exists=True))
 @click.argument("test", type=click.Path(exists=True))
 @click.option("--data-dset", default=DSET_DEFAULT)
-def validate(golden: str, test: str, data_dset: str) -> None:
+@click.option("--debug", is_flag=True)
+def validate(golden: str, test: str, data_dset: str, debug: bool) -> None:
     """Compare two HDF4 files for consistency."""
+    setup_logging(logger_name="disp_s1", debug=debug, filename=None)
     compare(golden, test, data_dset)

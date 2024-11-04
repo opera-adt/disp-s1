b3dfdaabd8080fd9e576c3757b52ef36e776923b
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2024-11-03 22:44:32 -0500

    Merge `release/0.4` change to validate script

--- src/disp_s1/cli/validate.py
+++ src/disp_s1/cli/validate.py
@@ -1,27 +1,15 @@
 import click
 from dolphin import setup_logging
 
-from disp_s1.validate import DSET_DEFAULT
-from disp_s1.validate import validate as _validate
+from disp_s1.validate import DSET_DEFAULT, compare
 
 
 @click.command()
+@click.argument("golden", type=click.Path(exists=True))
 @click.argument("test", type=click.Path(exists=True))
-@click.option("--golden", type=click.Path(exists=True))
-@click.option("--igram", type=click.Path(exists=True))
-@click.option("--json", type=click.Path(exists=True))
 @click.option("--data-dset", default=DSET_DEFAULT)
 @click.option("--debug", is_flag=True)
-def validate(
-    test: str,
-    golden: str | None,
-    igram: str | None,
-    json: str | None,
-    data_dset: str,
-    debug: bool,
-) -> None:
+def validate(golden: str, test: str, data_dset: str, debug: bool) -> None:
     """Validate an OPERA DISP-S1 product."""
     setup_logging(logger_name="disp_s1", debug=debug, filename=None)
-    _validate(
-        test, golden_file=golden, igram_file=igram, json_file=json, data_dset=data_dset
-    )
+    compare(golden, test, data_dset)

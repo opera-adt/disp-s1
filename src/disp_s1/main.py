e4bc789ed60826755fc24925891987fd6fd64386
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2025-02-26 15:43:17 -0500

    Use scale versions of timeseries paths for regrowing snaphu

--- src/disp_s1/main.py
+++ src/disp_s1/main.py
@@ -26,9 +26,11 @@ from disp_s1.pge_runconfig import AlgorithmParameters, RunConfig
 
 from ._reference import ReferencePoint, read_reference_point
 from ._utils import (
+    METERS_TO_RADIANS,
     _create_correlation_images,
     _update_snaphu_conncomps,
     _update_spurt_conncomps,
+    create_scaled_vrt,
 )
 
 logger = logging.getLogger(__name__)
@@ -205,12 +207,15 @@ def create_products(
     # Check and update connected components paths
     assert out_paths.conncomp_paths is not None
     if set(group_by_date(out_paths.conncomp_paths).keys()) != disp_date_keys:
+        timeseries_rad_paths = create_scaled_vrt(
+            out_paths.timeseries_paths, scale_factor=METERS_TO_RADIANS
+        )
         method = cfg.unwrap_options.unwrap_method
         if method in ("snaphu", "phass", "whirlwind"):
             row_looks, col_looks = cfg.phase_linking.half_window.to_looks()
             nlooks = row_looks * col_looks
             out_paths.conncomp_paths = _update_snaphu_conncomps(
-                timeseries_paths=out_paths.timeseries_paths,
+                timeseries_paths=timeseries_rad_paths,
                 stitched_cor_paths=out_paths.stitched_cor_paths,
                 mask_filename=combined_mask_file,
                 unwrap_options=cfg.unwrap_options,
@@ -218,6 +223,7 @@ def create_products(
             )
         elif method == "spurt":
             out_paths.conncomp_paths = _update_spurt_conncomps(
+                # We don't need the scaled-to-radians version here:
                 timeseries_paths=out_paths.timeseries_paths,
                 template_conncomp_path=out_paths.conncomp_paths[0],
             )

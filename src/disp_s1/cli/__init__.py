f81e1e0a3979bdb6843b4bae42a6ef74e8d0cfbb
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2025-03-19 22:31:03 -0400

    Add basic validation for static layers workflow (#294)
    * Add basic validation for static layers workflow
    
    * Also create COG overviews

--- src/disp_s1/cli/__init__.py
+++ src/disp_s1/cli/__init__.py
@@ -3,7 +3,7 @@ import click
 from .download import download_group
 from .make_browse import make_browse
 from .run import run_cli
-from .validate import validate
+from .validate import validate, validate_static_layers
 
 
 @click.group(name="disp-s1")
@@ -19,6 +19,7 @@ def cli_app(ctx: click.Context, debug: bool) -> None:
 
 cli_app.add_command(run_cli)
 cli_app.add_command(validate)
+cli_app.add_command(validate_static_layers)
 cli_app.add_command(make_browse)
 cli_app.add_command(download_group)
 
--- src/disp_s1/cli/validate.py
+++ src/disp_s1/cli/validate.py
@@ -1,7 +1,8 @@
-import click
-from dolphin import setup_logging
+from pathlib import Path
 
-from disp_s1.validate import DSET_DEFAULT, compare
+import click
+
+from disp_s1.validate import DSET_DEFAULT, compare, compare_static_layers
 
 
 @click.command()
@@ -11,5 +12,19 @@ from disp_s1.validate import DSET_DEFAULT, compare
 @click.option("--debug", is_flag=True)
 def validate(golden: str, test: str, data_dset: str, debug: bool) -> None:
     """Validate an OPERA DISP-S1 product."""
+    from dolphin import setup_logging
+
     setup_logging(logger_name="disp_s1", debug=debug, filename=None)
     compare(golden, test, data_dset)
+
+
+@click.command()
+@click.argument("golden", type=click.Path(exists=True))
+@click.argument("test", type=click.Path(exists=True))
+@click.option("--debug", is_flag=True)
+def validate_static_layers(golden: str, test: str, debug: bool) -> None:
+    """Validate the OPERA DISP-S1-STATIC products."""
+    from dolphin import setup_logging
+
+    setup_logging(logger_name="disp_s1", debug=debug, filename=None)
+    compare_static_layers(Path(golden), Path(test))
